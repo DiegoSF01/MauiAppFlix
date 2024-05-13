e80a5d3afee03cdf2ff348d87758dfa414409303
DiegoSF01 <diegosouzafurlanetto@gmail.com>
2024-05-13 15:59:47 -0300

    NetFlix feita com Maui.

---
+++ MauiAppFlix/Menu.xaml.cs
@@ -0,0 +1,29 @@
+namespace MauiAppFlix;
+
+public partial class Menu : ContentPage
+{
+	public Menu()
+	{
+		InitializeComponent();
+	}
+
+    private void acao_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void animacao_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void aventura_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void comedia_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
