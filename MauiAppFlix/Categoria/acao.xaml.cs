33f373a955134851d407af2a67cc67a265b0c3fc
DiegoSF01 <diegosouzafurlanetto@gmail.com>
2024-05-27 16:01:04 -0300

    Netflix feita com Maui.

--- MauiAppFlix/Categoria/acao.xaml.cs
+++ MauiAppFlix/Categoria/acao.xaml.cs
@@ -9,21 +9,53 @@ public partial class acao : ContentPage
 
     private void btnjurassic_park_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.acao.jurassic_park());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
-    private void btnkarate_kid_Clicked(System.Object sender, System.EventArgs e)
+    private void btnkarate_kid_Clicked(Object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.acao.karate_kid());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
-    private void btnvelozes_e_furiosos_Clicked(System.Object sender, System.EventArgs e)
+    private void btnvelozes_e_furiosos_Clicked(Object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.acao.velozes_e_furiosos());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
-    private void btnvingadores_ultimato_Clicked(System.Object sender, System.EventArgs e)
+    private void btnvingadores_ultimato_Clicked(Object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.acao.vingadores_ultimato());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
