cbee428f0ffce5b96cf8133319e8abc7cd40f87b
DiegoSF01 <diegosouzafurlanetto@gmail.com>
2024-05-28 12:26:49 -0300

    Netflix feita com Maui.

--- MauiAppFlix/Categoria/animacao.xaml.cs
+++ MauiAppFlix/Categoria/animacao.xaml.cs
@@ -9,21 +9,53 @@ public partial class animacao : ContentPage
 
     private void btnnem_que_a_vaca_tussa_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.animacao.nem_que_a_vaca_tussa());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btntoy_story_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.animacao.toy_story());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnrio_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.animacao.rio());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private void btnrobos_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            Navigation.PushAsync(new Filmes.animacao.robos());
 
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
